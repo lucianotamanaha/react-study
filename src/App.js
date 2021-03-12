4f1195bd5d0ca502b735285724f3ffed4c1b55c3
lucianotamanaha <lucianotamanaha@gmail.com>
2021-03-12 15:25:33 -0300

    Course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Courses from './containers/Courses/Courses';
+import Users from './containers/Users/Users';
+import NoMatch from './component/NoMatch/NoMatch'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <nav>
+          <ul style={{listStyle: 'none', margin: 'auto', padding: '0'}}>
+            <li style={{margin: '10px', display: 'inline-block'}}>
+              <NavLink to='/courses'>Courses</NavLink>
+            </li>
+            <li style={{margin: '10px', display: 'inline-block'}}>
+              <NavLink to='/users'>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/users" component={Users}/>
+          <Route path="/courses" component={Courses}/>
+          <Redirect from='/all-course' to='/courses'/>
+          <Route component={NoMatch}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/NoMatch/NoMatch.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NoMatch = () => (
+    <h1 style={{textAlign:'center'}}>Page not Found</h1>
+)
+
+export default NoMatch;
