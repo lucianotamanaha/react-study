57a54a460c1d8bdd3dc9d60a6ac2a7146a1e450b
lucianotamanaha <lucianotamanaha@gmail.com>
2021-03-29 14:27:45 -0300

    section 14

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
-import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 
-import Courses from './containers/Courses/Courses';
-import Users from './containers/Users/Users';
-import NoMatch from './component/NoMatch/NoMatch'
+import Counter from './containers/Counter/Counter';
+import './App.css';
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div className="App">
-        <nav>
-          <ul style={{listStyle: 'none', margin: 'auto', padding: '0'}}>
-            <li style={{margin: '10px', display: 'inline-block'}}>
-              <NavLink to='/courses'>Courses</NavLink>
-            </li>
-            <li style={{margin: '10px', display: 'inline-block'}}>
-              <NavLink to='/users'>Users</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/users" component={Users}/>
-          <Route path="/courses" component={Courses}/>
-          <Redirect from='/all-course' to='/courses'/>
-          <Route component={NoMatch}/>
-        </Switch>
+       <Counter />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducer from './store/reducer';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
